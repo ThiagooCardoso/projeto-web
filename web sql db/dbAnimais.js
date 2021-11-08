145339e2d7b6f06417a681dad749fc83eb7e1cb8
ThiagooCardoso <taugusto964@gmail.com>
2021-11-08 11:17:33 -0300

    alteraçoes db

--- web sql db/dbAnimais.js
+++ web sql db/dbAnimais.js
@@ -1,16 +1,54 @@
     //criar ou abrir database
-    var Database_Name = 'AnimaisDatabase';    
+    var Database_Name = 'AnimaisDatabase';   
     var Version = 1.0;    
     var Text_Description = 'Database dos Animais';    
     var Database_Size = 2 * 1024 * 1024;    
-    var dbObj = openDatabase(Database_Name, Version, Text_Description, Database_Size, OnSuccessCreate());  
+    var dbObj = openDatabase(Database_Name, Version, Text_Description, Database_Size, OnSuccessCreate()); 
+    
+    function OnSuccessCreate() {    
+        console.log('Database Created Sucessfully');    
+    }  
+   // import {db}from 'projeto-web\web sql db\dbDieta.js';
+    
+     
+
 
   
-           
-function OnSuccessCreate() {    
-    console.log('Database Created Sucessfully');    
-}  
+window.addEventListener('load',createSelect()) ;         
+    
 
+//inserir banco de dados em um select
+function createSelect() {
+
+    
+    dbObj.transaction(function(tx) {
+        //Busca os valores Banco de dados 
+        tx.executeSql('SELECT * FROM Cadastro_Dietas ', [], function(a, result) {
+            var rows = result.rows;
+            // Inicia a string com valores necessários para montar o select
+            var options = ` <label for="Dietas"></label>
+                            <select class="form-control" id="diet" name="Dietas">
+                            <option></option>`;
+
+
+
+
+
+            // Insere cada usuario do banco de dados como uma opção no select
+            for (var i = 0; i < rows.length; i++) {
+                options += `<option>${rows[i].nomeDieta}</option>`;
+            }
+
+            // Finaliza a string necessaria para montar o select
+            options += `</select>`;
+
+            // Insere o select montado na div
+            document.getElementById("Select").innerHTML += options;
+
+
+        },null);
+    });
+}
 function Insert() { 
     //cria tabela
     dbObj.transaction(function (tx) {    
--- web sql db/dbDieta.js
+++ web sql db/dbDieta.js
@@ -1,41 +1,83 @@
-//criar database e abrir
+//Criar database e abrir
 var Database_Name = 'DietasDatabase';    
 var Version = 1.0;    
 var Text_Description = 'Database das Dietas';    
 var Database_Size = 2 * 1024 * 1024;    
-var dbObj = openDatabase(Database_Name, Version, Text_Description, Database_Size, OnSuccessCreate());  
+var db = openDatabase(Database_Name, Version, Text_Description, Database_Size, OnSuccessCreate());  
+
+export {db};
 
            
 function OnSuccessCreate() {    
     console.log('Database Created Sucessfully');    
 } 
 
-function Insert(){
-    //cria tabela
-    dbObj.transaction (function(tx){
-        tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro_Dietas (id integer primary key asc, nomeDieta string, QuantidadeInsumos integer, NomeInsumo string, Quantidade integer, duração integer)',
-        [],
-        function (){console.log("Tabela criada com sucesso!")},
-        function (){console.log ("Erro ao criar tabela!!")}  
-        );
-    }   
+
+ //cria tabela
+ db.transaction (function(tx){
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Cadastro_Dietas (id integer primary key asc, nomeDieta string, QuantidadeInsumos integer)',
+    [],
+    function (){console.log("Tabela criada com sucesso!")},
+    function (){console.log ("Erro ao criar tabela!!")}  
     );
+}   
+);
 
-    //pega valor dos inputs do formulario
-    var dieta = document.getElementById("dietname").value;
-    var qtdinsumos = document.getElementById("Quantidadeinsumos").value;
-    var nomeinsumo = document.getElementById("supplyname").value;
-    var quantidade = document.getElementById("quantity").value;
-    var duracao = document.getElementById("duration").value;
+ 
 
-    //inserir na tabela
-    dbObj.transaction (function(tx){
-        tx.executeSql ('INSERT INTO Cadastro_Dietas(nomeDieta, QuantidadeInsumos, NomeInsumo, Quantidade, duração) values(?,?,?,?,?)',
-        [dieta,qtdinsumos,nomeinsumo,quantidade,duracao],
-        function(){alert("tabela preenchida!")},
-        null
+function Insert(){ 
+
+       //inserir na tabela
+       var dieta = document.getElementById("dietname").value; 
+       var qtdinsumos = document.getElementById("Quantidadeinsumos").value;
+   db.transaction (function(tx){
+       tx.executeSql ('INSERT INTO Cadastro_Dietas(nomeDieta, QuantidadeInsumos) values(?,?)',
+       [dieta,qtdinsumos],
+       function(){alert("tabela preenchida!")},
+       null
+       );
+   }
+   );
+    
+    //adicionar colunas na tabela 
+    for ( var i = 1; i <qtdinsumos;i++){
+
+        var nomeinsumo = document.getElementById("supplyname"+i).value;
+        var quantidade = document.getElementById("quantity"+i).value;
+        var duracao = document.getElementById("duration"+i).value;
+
+        db.transaction (function(tx){
+            tx.executeSql('ALTER TABLE Cadastro_Dietas ADD NomeInsumo('+i+') VARCHAR(255) NOT NULL DEFAULT',
+            [],
+            function (){console.log("Colunas nome insumo adicionada  com sucesso!")},
+            function (){console.log ("Erro ao ADICIONAR COLUNAS tabela!!")}  
+            );
+            tx.executeSql('ALTER TABLE  Cadastro_Dietas ADD Quantidade('+i+') INTEGER NOT NULL DEFAULT',
+            [],
+            function (){console.log("Colunas  quantidade adicionada  com sucesso!")},
+            function (){console.log ("Erro ao ADICIONAR COLUNAS tabela!!")}  
+            );
+            tx.executeSql('ALTER TABLE Cadastro_Dietas ADD Duração('+i+') INTEGER NOT NULL DEFAULT',
+            [],
+            function (){console.log("Colunas  Duração adicionada  com sucesso!")},
+            function (){console.log ("Erro ao ADICIONAR COLUNAS tabela!!")}  
+            );
+        }   
         );
+        
+        //inserir na tabela
+        
+        db.transaction (function(tx){
+            tx.executeSql ('INSERT INTO Cadastro_Dietas(NomeInsumo('+i+'),Quantidade('+i+'),Duração('+i+')) values(?,?,?)',
+            [nomeinsumo,quantidade,duracao],
+            function(){alert("tabela foi  preenchida!")},
+            function(){alert("valores nao inseridos")}
+            );
+        }
+        );
+    
     }
-    );
 }
-
+    
+    
+    
